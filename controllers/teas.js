7a58dfb8afd114ecfe1ae16d7a6b6d6424535721
Sabika Ali <Sabikaalghadeeri@gmail.com>
2022-10-26 14:56:05 +0300

    update2~26\10

--- controllers/teas.js
+++ controllers/teas.js
@@ -13,11 +13,29 @@ router.get('/', (req, res) => {
       if (!tea) throw Error()
     //   console.log(tea.type)
     //   res.send(tea)
-      res.render('teas/show', { tea: tea })
+      res.render('teas/index', { tea: tea })
     })
     .catch((error) => {
       console.log(error)
     })
   })
+
+  router.get('/:id', (req, res) => {
+    db.tea.findOne({
+      where: { id: req.params.id },
+      include: [db.comment]
+    })
+    .then((response) => {
+      if (!response) throw Error()
+    //   console.log(tea.type)
+    //   res.send(response)
+      res.render('teas/show', { tea: response })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  })
+
+
 
 module.exports = router
